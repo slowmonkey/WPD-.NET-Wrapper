7b4d641c846153b57b9eb578904d8774fef07faa
Gavin Chin <chinoishin+github@gmail.com>
2012-05-02 00:47:07 +0100

    Removed commented out code.

--- WindowsPortableDeviceNet/Model/Properties/ContentTypeProperty.cs
+++ WindowsPortableDeviceNet/Model/Properties/ContentTypeProperty.cs
@@ -9,9 +9,6 @@ namespace WindowsPortableDeviceNet.Model.Properties
     /// </summary>
     public class ContentTypeProperty : BaseWPDProperties
     {
-        //private const string WPD_CONTENT_TYPE_FOLDER_OBJECT = "27E2E392-A111-48E0-AB0C-E17705A05F85";
-        //private const string WPD_CONTENT_TYPE_FUNCTIONAL_OBJECT = "99ED0160-17FF-4C44-9D98-1D7A6F941921";
-        //private const string WPD_CONTENT_TYPE_IMAGE = "EF2107D5-A52A-4243-A26B-62D4176D7603";
         private static readonly Dictionary<string, WindowsPortableDeviceEnumerators.ContentType> WpdContentTypeGuids =
             new Dictionary<string, WindowsPortableDeviceEnumerators.ContentType>
                 {
