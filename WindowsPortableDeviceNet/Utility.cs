a6e05d44155d734d7f8675cea68648b7d1d2def0
gavin.chin <slowmonkeyking@gmail.com>
2012-12-19 19:02:53 +0000

    Refactored code to match other refactored code.

--- WindowsPortableDeviceNet/Utility.cs
+++ WindowsPortableDeviceNet/Utility.cs
@@ -1,71 +1,66 @@
-﻿using System.Collections.Generic;
-using PortableDeviceApiLib;
+﻿using System;
 using WindowsPortableDeviceNet.Model;
-using System;
 
 namespace WindowsPortableDeviceNet
 {
     public class Utility
     {
-        public List<Device> Get()
+        public DeviceCollection GetConnectedPortableDevices()
         {
-            List<Device> connectedPortableDevices = new List<Device>();
-            PortableDeviceManager manager = new PortableDeviceManager();
-
-            manager.RefreshDeviceList();
-            uint count = 1;
-            manager.GetDevices(null, ref count);
-
-            if (count == 0) return connectedPortableDevices;
-
-            // Call the above again because we now know how many devices there are.
-
-            string[] deviceIds = new string[count];
-            manager.GetDevices(ref deviceIds[0], ref count);
-
-            ExtractDeviceInformation(deviceIds, connectedPortableDevices);
+            DeviceCollection connectedPortableDevices = new DeviceCollection();
+            connectedPortableDevices.LoadConnectedDevices();
             return connectedPortableDevices;
         }
 
-        private void ExtractDeviceInformation(string[] deviceIds, List<Device> connectedPortableDevices)
-        {
-            foreach (string deviceId in deviceIds)
-            {
-                connectedPortableDevices.Add(new Device(deviceId)); 
-            }            
-        }
-
-
         public void CopyToPC(string deviceName, string source, string destination, bool overwrite)
         {
-            FindTargetDevice(deviceName);
-            _Device.Connect();
-            _Device.CopyToPC(source, destination, overwrite);
-        }
-
-        public void CopyToDevice(string device, string source, string destination, bool overwrite)
-        {
-            FindTargetDevice(device);
-            _Device.Connect();
-            _Device.CopyToDevice(source, destination, overwrite);
-        }
-
-        private void FindTargetDevice(string deviceName)
-        {
-            List<Device> devices = this.Get();
-            foreach (Device device in devices)
-            {
-                if (device.FriendlyName.Value == deviceName)
+            //if (copyDataDetails == null) throw new ArgumentNullException("copyDataDetails");
+            CopyDataDetails copyDataDetails =
+                new CopyDataDetails
                 {
-                    _Device = device;
-                }
-            }
-            if (_Device == null)
-            {
-                throw new Exception("target device not connected!");
-            }
+                    DeviceName = deviceName,
+                    SourcePath = source,
+                    DestinationPath = destination,
+                    IsOverwrite = overwrite
+                };
+
+            Device device = GetConnectedPortableDevices().GetDevice(deviceName);
+
+            ValidateDeviceIsConnected(deviceName, device);
+
+            device
+                .Connect()
+                .CopyFromDevice(copyDataDetails);
         }
 
-        private Device _Device;
+        public void CopyToDevice(string deviceName, string source, string destination, bool overwrite)
+        {
+            //if (copyDataDetails == null) throw new ArgumentNullException("copyDataDetails");
+            CopyDataDetails copyDataDetails = 
+                new CopyDataDetails
+                    {
+                        DeviceName = deviceName,
+                        SourcePath = source,
+                        DestinationPath = destination,
+                        IsOverwrite = overwrite
+                    };
+
+            Device device = GetConnectedPortableDevices().GetDevice(deviceName);
+
+            ValidateDeviceIsConnected(deviceName, device);
+
+            device
+                .Connect()
+                .CopyToDevice(copyDataDetails);
+        }
+
+        private static void ValidateDeviceIsConnected(string deviceName, Device device)
+        {
+            if (device != null) return;
+
+            string errorMessage = String.Format("Target device: \"{0}\" is not connected!", deviceName);
+            throw new Exception(errorMessage);
+        }
+
     }
 }
