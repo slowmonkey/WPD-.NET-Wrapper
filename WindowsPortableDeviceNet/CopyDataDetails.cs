9cf3d9ec8876c112e22f16a96aaa78e26c530a3c
gavin.chin <slowmonkeyking@gmail.com>
2012-12-19 19:03:03 +0000

    Adding new file.

---
+++ WindowsPortableDeviceNet/CopyDataDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace WindowsPortableDeviceNet
+{
+    public class CopyDataDetails
+    {
+        public string DeviceName { get; set; }
+        public string SourcePath{ get; set; }
+        public string DestinationPath { get; set; }
+        public bool IsOverwrite { get; set; }
+        public bool IsKeepFolderStructure { get; set; }
+
+        public CopyDataDetails()
+        {
+            DeviceName = String.Empty;
+            SourcePath = String.Empty;
+            DestinationPath = String.Empty;
+            IsOverwrite = false;
+            IsKeepFolderStructure = true;
+        }
+    }
+}
