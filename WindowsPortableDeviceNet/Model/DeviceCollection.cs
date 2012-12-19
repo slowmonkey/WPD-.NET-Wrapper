2c6202f8d031d370db3416a9a2a4d0f824239010
gavin.chin <slowmonkeyking@gmail.com>
2012-12-19 18:55:49 +0000

    New collection classes for device and item
    To manage any operations on the set of devices or items.

---
+++ WindowsPortableDeviceNet/Model/DeviceCollection.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using PortableDeviceApiLib;
+
+namespace WindowsPortableDeviceNet.Model
+{
+    public class DeviceCollection : List<Device>
+    {
+        /// <summary>
+        /// This static method loads a list of connected devices.
+        /// </summary>
+        /// <returns></returns>
+        public void LoadConnectedDevices()
+        {
+            PortableDeviceManager manager = new PortableDeviceManager();
+
+            manager.RefreshDeviceList();
+            uint count = 1;
+            manager.GetDevices(null, ref count);
+
+            if (count == 0) return;
+
+            // Call the above again because we now know how many devices there are.
+
+            string[] deviceIds = new string[count];
+            manager.GetDevices(ref deviceIds[0], ref count);
+
+            ExtractDeviceInformation(deviceIds);
+        }
+
+        public Device GetDevice(string deviceName)
+        {
+            foreach (Device device in this)
+            {
+                if (device.FriendlyName.Value == deviceName) return device;
+            }
+
+            return null;
+        }
+
+        private void ExtractDeviceInformation(string[] deviceIds)
+        {
+            foreach (string deviceId in deviceIds)
+            {
+                Add(new Device(deviceId));
+            }
+        }
+
+    }
+}
---
+++ WindowsPortableDeviceNet/Model/ItemCollection.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WindowsPortableDeviceNet.Model
+{
+    public class ItemCollection : List<Item>
+    {
+        public void CopyFromDevice(CopyDataDetails copyDataDetails)
+        {
+            if (String.IsNullOrEmpty(copyDataDetails.SourcePath)) throw new ApplicationException("CopyFromDevice(): No source path is specified");
+
+            string[] sourcePathItems = copyDataDetails.SourcePath.Split('\\');
+            bool transferAllItems = false;
+            if (sourcePathItems.Length == 1) transferAllItems = true;
+
+            foreach (Item item in this)
+            {
+                if (item.Name.ToString() != sourcePathItems[0]) continue;
+
+                if (transferAllItems)
+                {
+                    item.TransferFiles(copyDataDetails);
+                }
+                else
+                {
+                    // Remove the first source path item from the source string.
+
+                    copyDataDetails.SourcePath = copyDataDetails.SourcePath.Remove(0, sourcePathItems[0].Length + 1);
+                    item.TransferFiles(copyDataDetails);
+                }
+                return;
+            }
+        }
+
+        public string FindParentObjectId(string destinationPath)
+        {
+            string[] destinationPathItems = destinationPath.Split('\\');
+            Item item = FindItemByName(destinationPathItems[0]);
+
+            if (destinationPathItems.Length == 1)
+            {
+                if (item != null) return item.Id;
+                return String.Empty;
+            }
+
+            if ((destinationPathItems.Length > 1) && item.ContentType.IsFolder())
+            {
+                if (item == null) return String.Empty;
+                    
+                return item.FindParentObjectId(destinationPath.Remove(0, destinationPathItems[0].Length + 1));                    
+            }
+
+            return String.Empty;
+        }
+
+        private Item FindItemByName(string name)
+        {
+            return this.Find(i => i.Name.Value == name);
+        }
+    }
+}
