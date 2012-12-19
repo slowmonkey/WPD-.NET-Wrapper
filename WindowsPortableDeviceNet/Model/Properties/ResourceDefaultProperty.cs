f146e9f7dd3f76cf5e7f6b25d53f36772171fa29
gavin.chin <slowmonkeyking@gmail.com>
2012-12-19 18:53:37 +0000

    New property definitions

---
+++ WindowsPortableDeviceNet/Model/Properties/ResourceDefaultProperty.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using PortableDeviceApiLib;
+
+namespace WindowsPortableDeviceNet.Model.Properties
+{
+    public class ResourceDefaultProperty : BaseWPDProperties
+    {
+        public ResourceDefaultProperty(IPortableDeviceValues deviceProperties)
+            : base(deviceProperties)
+        {
+            FormatId = new Guid(0xE81E79BE, 0x34F0, 0x41BF, 0xB5, 0x3F, 0xF1, 0xA0, 0x6A, 0xE8, 0x78, 0x42);
+            PositionId = 0;
+        }
+
+        public ResourceDefaultProperty()
+        {
+            FormatId = new Guid(0xE81E79BE, 0x34F0, 0x41BF, 0xB5, 0x3F, 0xF1, 0xA0, 0x6A, 0xE8, 0x78, 0x42);
+            PositionId = 0;
+        }
+    }
+}
---
+++ WindowsPortableDeviceNet/Model/Properties/WpdObjectParentIdProperty.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using PortableDeviceApiLib;
+
+namespace WindowsPortableDeviceNet.Model.Properties
+{
+    public class WpdObjectParentIdProperty : BaseWPDProperties
+    {
+        public WpdObjectParentIdProperty(IPortableDeviceValues deviceProperties)
+            : base(deviceProperties)
+        {
+            FormatId = new Guid(0xEF6B490D, 0x5CD8, 0x437A, 0xAF, 0xFC, 0xDA, 0x8B, 0x60, 0xEE, 0x4A, 0x3C);
+            PositionId = 3;
+        }
+
+        public WpdObjectParentIdProperty()
+        {
+            FormatId = new Guid(0xEF6B490D, 0x5CD8, 0x437A, 0xAF, 0xFC, 0xDA, 0x8B, 0x60, 0xEE, 0x4A, 0x3C);
+            PositionId = 3;
+        }
+    }
+}
