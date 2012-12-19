906e1c438e45b28169cb74fd42e95f8b12c9dee6
gavin.chin <slowmonkeyking@gmail.com>
2012-12-19 19:02:15 +0000

    General changes based on other code changes.

---
+++ WindowsPortableDeviceNet.Test/ItemTestSuite.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WindowsPortableDeviceNet.Test
+{
+    [TestClass]
+    public class ItemTestSuite
+    {
+        [TestMethod]
+        public void TransferFiles_TestAllContentTypes()
+        {
+            Assert.Fail("Not Implemented");
+        }
+    }
+}
---
+++ WindowsPortableDeviceNet.Test/UtilityTestSuite.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WindowsPortableDeviceNet.Model;
+
+namespace WindowsPortableDeviceNet.Test
+{
+    [TestClass]
+    public class UtilityTestSuite
+    {
+        [TestMethod]
+        [TestCategory("Integration")]
+        public void Get_GetConnectedDevices_1DeviceDetected()
+        {
+            MessageBox.Show(
+                "Please ensure that a camera is connected.",
+                "Test Message",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Stop);
+            Utility utility = new Utility();
+            List<Device> device = utility.GetConnectedPortableDevices();
+
+            Assert.AreEqual(1, device.Count);
+        }
+
+        [TestMethod]
+        [TestCategory("Integration")]
+        public void TransferData_WithNoFolderStructure_OnlyFilesCopied()
+        {
+            MessageBox.Show(
+                "Please ensure that a camera is connected.",
+                "Test Message",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Stop);
+            Utility utility = new Utility();
+            List<Device> device = utility.GetConnectedPortableDevices();
+            device[0].Connect();
+            device[0].TransferData("C:\\Users\\Gav\\Desktop\\test", isOverwrite: false, isKeepFolderStructure: false);
+            device[0].Disconnect();
+        }
+
+        [TestMethod]
+        [TestCategory("Integration")]
+        public void TransferData_WithFolderStructure_FilesCopiedWithFolderStructure()
+        {
+            MessageBox.Show(
+                "Please ensure that a camera is connected.",
+                "Test Message",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Stop);
+            Utility utility = new Utility();
+            List<Device> device = utility.GetConnectedPortableDevices();
+            device[0].Connect();
+            device[0].TransferData("C:\\Users\\Gav\\Desktop\\test", isOverwrite: false, isKeepFolderStructure: true);
+            device[0].Disconnect();
+        }
+    }
+}
